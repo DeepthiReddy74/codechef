519542c042cde3bd2478eab26ecef625a4e5cb0c
DeepthiReddy74 <reddydeepthi68699@gmail.com>
2024-12-14 15:51:51 +0530

    Add files via upload

---
+++ CLEARDAY.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner Days = new Scanner(System.in);
+        int X = Days.nextInt();
+        int Y = Days.nextInt();
+        int totaldays = 7;
+        System.out.println(totaldays - X - Y);
+
+	}
+}
---
+++ DOUBLERENT.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner inirent = new Scanner(System.in);
+        int X = inirent.nextInt();
+        System.out.println(X * 2);
+
+	}
+}
